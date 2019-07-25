034b5d7ea3672aabc905362761bc7d8b178e887e
mengi2111 <52619255+mengi2111@users.noreply.github.com>
2019-07-24 21:15:15 -0400

    Add files via upload

---
+++ github/assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import {bookInfo} from '../../app/bookInfo';
+
+export const MYBOOKS: bookInfo[] = [
+  {sname: "Don Quixote", sauthor: "Miguel de Cervantes", sgenre: "Novel", syearpublished: "1605", spicture: "assets/images/DQ.jpg"},
+  {sname: "Harry Potter", sauthor: "J.K. Rowling", sgenre: "Fantasy", syearpublished: "1997", spicture: "assets/images/HP.jpg"},
+  {sname: "Longitude", sauthor: "Dava Sobel", sgenre: "Geography", syearpublished: "1995", spicture: "assets/images/L.jpg"},
+  {sname: "The Great Gatsby", sauthor: "F.Scott Fitzgerland", sgenre: "Novel", syearpublished: "1925", spicture: "assets/images/TGG.jpeg"}
+
+]
